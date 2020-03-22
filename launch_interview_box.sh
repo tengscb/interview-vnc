e1e1a7d6d9d5165f3b21a166da7d14469e720e75
Bill Sun <teng.sg.scb@gmail.com>
2020-03-22 16:30:43 +0800

    removed proxy setting (unable to get http://localhost:9090 work)

--- launch_interview_box.sh
+++ launch_interview_box.sh
@@ -61,10 +61,6 @@ docker run -d -it \
 -e VNC_PASSWORD=${VNC_PWD} \
 -e VIDEO=true \
 -e VIDEO_FILE_NAME=interview_recording \
--e HTTP_PROXY=http://127.0.0.1:9090 \
--e HTTPS_PROXY=http://127.0.0.1:9090 \
--e NO_PROXY=localhost,127.0.0.1 \
--e no_proxy=localhost,127.0.0.1 \
 billsun/coding-interview:$TAG
 
 apt-get install git
